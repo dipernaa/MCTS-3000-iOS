98e60b5f1317665e6e46195e43e4bd8d3e260242
Tony DiPerna <adiperna@penrodsoftware.com>
2016-04-21 11:49:58 -0500

    day 1

---
+++ MCTS3000/RouteCell.swift
@@ -0,0 +1,15 @@
+//
+//  RouteCell.swift
+//  MCTS3000
+//
+//  Created by Tony DiPerna on 4/21/16.
+//  Copyright © 2016 MSOE SE-4910i. All rights reserved.
+//
+
+import Foundation
+
+class RouteCell: UITableViewCell {
+    
+    @IBOutlet weak var lblNumber: UILabel!
+    @IBOutlet weak var lblName: UILabel!
+}
--- MCTS3000/RoutesViewController.swift
+++ MCTS3000/RoutesViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 import Alamofire
 import ObjectMapper
 
-class RoutesViewController: UIViewController {
+class RoutesViewController: UITableViewController {
+    
+    var routes: [RouteModel]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,26 @@ class RoutesViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return routes?.count ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCellWithIdentifier("RouteCell", forIndexPath: indexPath) as? RouteCell else {
+            fatalError("THIS SHOULD NEVER HAPPEN")
+        }
+        
+        let route = routes?[indexPath.row]
+        
+        cell.lblTitle.text = route?.number ?? "no route number"
+        return cell
+    }
 
     func loadRoutes() {
         
@@ -30,7 +52,11 @@ class RoutesViewController: UIViewController {
             guard let object = object as? [String: AnyObject] else {
                 return
             }
-            print(object)
+            
+            let results = object["bustime-response"]
+            let acutalResults = results!["routes"]
+            self?.routes = Mapper<RouteModel>().mapArray(acutalResults)
+            print(acutalResults)
         }
     }
 
