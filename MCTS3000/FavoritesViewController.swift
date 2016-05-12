39465b2c23786fa3eb735d4c29311b473418703a
Tony DiPerna <adiperna@penrodsoftware.com>
2016-05-12 09:18:39 -0500

    able to see stops

--- MCTS3000/FavoritesViewController.swift
+++ MCTS3000/FavoritesViewController.swift
@@ -11,12 +11,12 @@ import RealmSwift
 
 class FavoritesViewController: UITableViewController {
     
-    let realm = try! Realm()
-    lazy var stops: Results<Stop> = { self.realm.objects(Stop) }()
+    
+    var stops: Results<Stop>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.reloadData()
+        loadStops()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,15 +25,32 @@ class FavoritesViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stops?.count ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCellWithIdentifier("StopCell", forIndexPath: indexPath) as? StopCell else {
             fatalError("THIS SHOULD NEVER HAPPEN")
         }
         
-        let stop = stops[indexPath.row]
-        print("\(stop.stopName)")
-        cell.lblName.text = stop.stopName
+        let stop = stops?[indexPath.row]
+        cell.lblName.text = stop?.stopName
         return cell
+    }
+    
+    func loadStops() {
+        let realm = try! Realm()
+        stops = { realm.objects(Stop) }()
+        tableView.reloadData()
     }
 }
 
